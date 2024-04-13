da0fa8b9dc7970dddea635c8b716dc13165b703e
Ho Nyeong <dev.honing@gmail.com>
2024-04-13 21:49:35 +0900

    Feat: 인사를 위한 Greet() 함수 정의

--- test.py
+++ test.py
@@ -5,3 +5,11 @@
 # 3. 나이가 10살 이상 20살 미만이면 "안녕하세요"라고 인사한다
 # 4. 그 외에는 "안녕하십니까"라고 인사한다
 
+def Greet(name, age):
+    if age < 10:
+        print("안녕, " + name + "야")
+    elif 10 <= age < 20:
+        print(name + "님," "안녕하세요!")
+    else:
+        print(name + "님", "안녕하십니까?")
+        
